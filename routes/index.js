70171e8b915bb01404ee78ed7ffe05d02ef7c5f4
agyadnkr <agyad.dinakara@gmail.com>
2022-01-26 08:51:07 +0700

    Login done

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const indexRouter = require('express').Router()
 
 const UserController = require('../controllers/user')
 
-// indexRouter.post('/login', UserController.login)
+indexRouter.post('/login', UserController.login)
 indexRouter.post('/register', UserController.register)
 
 module.exports = indexRouter
