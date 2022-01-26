1ad0da7feac08deef5a2dc9ec2e6f01ea2812fc5
agyadnkr <agyad.dinakara@gmail.com>
2022-01-26 21:06:45 +0700

    Fetch Location Done

---
+++ controllers/location.js
@@ -0,0 +1,34 @@
+const { User, Location, Image, Favourite } = require('../models')
+const { Op } = require('sequelize')
+
+class LocationController {
+  static async addLocation(req, res, next) {
+    try {
+      const { name, address, lattitude, longitude, mapsUrl, price } = req.body;
+
+      const result = await Location.create({
+        name,
+        address,
+        lattitude,
+        longitude,
+        mapsUrl,
+        price
+      })
+      res.status(201).json({ message: 'Success add location' })
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  static async fetchLocations (req, res, next) {
+    try {
+      const result = await Location.findAll();
+
+      res.status(200).json(result)
+    } catch (error) {
+      next(error)
+    }
+  }
+}
+
+module.exports = LocationController
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
 const indexRouter = require('express').Router()
 
 const UserController = require('../controllers/user')
+const LocationController = require('../controllers/location')
 
 indexRouter.post('/login', UserController.login)
 indexRouter.post('/register', UserController.register)
+
+indexRouter.post('/locations', LocationController.addLocation)
+indexRouter.get('/locations', LocationController.fetchLocations)
 
 module.exports = indexRouter
